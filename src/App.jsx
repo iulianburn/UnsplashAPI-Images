53f190d9eaf8aa44653901f90e8f9c1d69a9b897
iulianburn <bmifitness2@gmail.com>
2023-05-20 13:21:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ThemeToggke from './ThemeToggke'
+import SearchForm from './SearchForm'
+import Gallery from './Gallery'
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggke/>
+      <SearchForm/>
+      <Gallery/>
+    </main>
+  )
+}
+
+export default App
